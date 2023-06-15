31c8dd02d20e6b282f599e84113edf08ae49b7b4
artisanbr <github@artisan.dev.br>
2023-06-15 15:13:13 -0300

    1.3-beta

--- resources/views/frontend/sitemap/index.blade.php
+++ resources/views/frontend/sitemap/index.blade.php
@@ -4,28 +4,20 @@
  */
 ?>
 <?php print '<?xml version="1.0" encoding="UTF-8" ?>'; ?>
-<urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
+              xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    @foreach($sitemaps as $sitemap)
 
-        @foreach($sitemaps as $sitemap)
+        <sitemap>
 
-            <sitemap>
+            <loc>{{ $sitemap['loc'] }}</loc>
 
-                <loc>{{ $sitemap['loc'] }}</loc>
+            <lastmod>{{ $sitemap['lastmod'] }}</lastmod>
 
-                <lastmod>{{ $sitemap['lastmod'] }}</lastmod>
+        </sitemap>
 
-            </sitemap>
+    @endforeach
 
-        @endforeach
-
-    </sitemapindex>
-
-</urlset>
+</sitemapindex>
