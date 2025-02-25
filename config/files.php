03cb5583aa03b48562dd98628e7aba21753c15e0
artisanbr <github@artisan.dev.br>
2025-02-25 17:09:56 -0300

    v0.8.30.4 - bugfix(CustomListItem): Correção na tela de cadastro
    Em andamento:
    - Wordpress Import Tools (em andamento)
    - Melhoria nas Categorias (em andamento)
    - Melhoria no cadastro de Temas (em andamento)
    - Novo Gerenciador de arquivos no Tema (em andamento)
    - Nova gestão de Scripts vinculados ao carregamento do site no Tema (em andamento)

--- config/files.php
+++ config/files.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2023-2024. Tanda Interativa - Todos os Direitos Reservados
+ * Copyright (c) 2023-2025. Tanda Interativa - Todos os Direitos Reservados
  * Desenvolvido por Renalcio Carlos Jr.
  */
 
