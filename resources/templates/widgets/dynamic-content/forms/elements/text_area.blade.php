9dcb7174bfc735d5e26682cdb3a31b9b71e9f48c
renalcio <carlos@tanda.com.br>
2025-04-14 23:02:15 -0300

    v0.10.5 - feat(Forms): Campos obrigatórios
    Em andamento:
    - Wordpress Import Tools (em andamento)
    - Melhoria nas Categorias (em andamento)
    - Melhoria no cadastro de Temas (em andamento)
    - Novo Gerenciador de arquivos no Tema (em andamento)
    - Nova gestão de Scripts vinculados ao carregamento do site no Tema (em andamento)

--- resources/templates/widgets/dynamic-content/forms/elements/text_area.blade.php
+++ resources/templates/widgets/dynamic-content/forms/elements/text_area.blade.php
@@ -4,4 +4,4 @@
 
 {{--<label for="input_{{ $element->slug }}" class="form-label">{{ $element->title }}</label>--}}
 <textarea type="text" class="form-control {{ $element->css_class ?? '' }}" id="input_{{ $element->slug }}" name="{{ $element->slug }}"
-          placeholder="{{ $element->title }}"></textarea>
+          placeholder="{{ $element->title }}" {{ $element->required ? 'required' : '' }}></textarea>
--- resources/templates/widgets/dynamic-content/forms/elements/text_field.blade.php
+++ resources/templates/widgets/dynamic-content/forms/elements/text_field.blade.php
@@ -12,4 +12,4 @@
 ])--}}
 {{--<label for="input_{{ $element->slug }}" class="form-label">{{ $element->title }}</label>--}}
 <input type="text" class="form-control {{ $element->css_class ?? '' }}" id="input_{{ $element->slug ?? $ $slug }}" name="{{ $element->slug ?? $slug }}"
-       placeholder="{{ $element->title ?? $title }}"/>
+       placeholder="{{ $element->title ?? $title }}" {{ $element->required ? 'required' : '' }}/>
